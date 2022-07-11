3d429427b80e85ddd67000c1f6ce32564fdbb5fc
Johann Gomez C <johanngomez02@email.com>
2022-07-11 02:29:40 -0500

    :hammer: [CREATE] connection database stablished and generated migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PgDatabaseModule } from './frameworks/pg/pg-data.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
   }),
+  PgDatabaseModule
   ],
 })
 export class AppModule {}
